18f9c80455a33cd035fadd3d6284db002a65b278
Johannes Koggdal <koggdal@spotify.com>
2013-05-07 14:31:47 +0200

    Move jsdoc3 formula to use spotify jsdoc

--- Library/Formula/jsdoc3.rb
+++ Library/Formula/jsdoc3.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Jsdoc3 < Formula
   homepage 'http://usejsdoc.org/'
-  url 'https://github.com/jsdoc3/jsdoc/tarball/v3.1.0'
-  sha1 'ec4ac5f5d0dc5ebc3508d1714a8fcf15ebf65983'
+  url 'https://github.com/koggdal/jsdoc/tarball/805cb60c703c40030b35b46c9dcabf6ee977ff70'
+  sha1 '1b5d2a9d2353c5cfa410eb86dee294c6a6937490'
 
   def install
     libexec.install Dir['*']
